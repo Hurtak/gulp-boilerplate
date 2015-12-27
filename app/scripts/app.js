20dc45007058d0e273110a71afc9342649a7fd39
hurtak <petr.hurtak@gmail.com>
2015-12-27 22:55:36 +0100

    scripts: better example showing named/default exports/imports

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,6 @@
-import one from './one.js';
+import defaultImport from './module1.js';
+import {second as namedImport}  from './module2.js';
 
-console.log('a');
-one();
+console.log('console log from app.js');
+defaultImport();
+namedImport();
