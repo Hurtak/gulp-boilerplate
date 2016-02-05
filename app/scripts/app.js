6b05e0a26ebe2048da1041b12d86c0414297b815
hurtak <petr.hurtak@gmail.com>
2016-02-05 23:59:27 +0100

    format script with js standard style

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
-import defaultImport from './module1.js';
-import {second as namedImport}  from './module2.js';
+import defaultImport from './module1.js'
+import { second as namedImport } from './module2.js'
 
-console.log('console log from app.js');
-defaultImport();
-namedImport();
+console.log('console log from app.js')
+defaultImport()
+namedImport()
--- app/scripts/module1.js
+++ app/scripts/module1.js
@@ -1,3 +1,3 @@
 export default () => {
-	console.log('console log from module1.js as default export');
-};
+  console.log('console log from module1.js as default export')
+}
