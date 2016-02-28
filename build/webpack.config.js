fd682d7bacc90647ffe5a6789b999c7137f661e7
Petr Huřťák <petr.hurtak@gmail.com>
2016-02-28 20:36:19 +0100

    webpack: properly configure babel, add devel mode

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const __DEV__ = false
+
 module.exports = {
 
   entry: [
@@ -21,13 +23,40 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: [
-            'stage-0',
-            'es2015'
-          ],
+          presets: [],
           plugins: [
-            // 'transform-runtime',
-            'transform-strict-mode'
+            'check-es2015-constants',
+            'transform-es2015-arrow-functions',
+            'transform-es2015-block-scoped-functions',
+            'transform-es2015-block-scoping',
+            'transform-es2015-classes',
+            'transform-es2015-computed-properties',
+            'transform-es2015-destructuring',
+            'transform-es2015-for-of',
+            'transform-es2015-function-name',
+            'transform-es2015-literals',
+            'transform-es2015-modules-commonjs',
+            'transform-es2015-object-super',
+            'transform-es2015-parameters',
+            'transform-es2015-shorthand-properties',
+            'transform-es2015-spread',
+            'transform-es2015-sticky-regex',
+            'transform-es2015-template-literals',
+            'transform-es2015-typeof-symbol',
+            'transform-es2015-unicode-regex',
+
+            ...(__DEV__ ? [
+              'transform-async-to-generator'
+            ] : [
+              "transform-async-to-generator",
+              'transform-regenerator',
+              "transform-runtime",
+            ]),
+
+            'transform-exponentiation-operator',
+            'transform-object-rest-spread',
+
+            'transform-strict-mode',
           ],
           cacheDirectory: true
         }
